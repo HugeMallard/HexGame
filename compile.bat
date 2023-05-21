11195e823cff4894760a8c90003c497175fd91a2
Andrew Humphries <andrewhumphries7@gmail.com>
2023-05-21 14:07:10 +1000

    wip

---
+++ compile.bat
@@ -0,0 +1,2 @@
+nuitka --standalone --include-package=OpenGL --windows-icon-from-ico="starshot.ico" main.py
+:: nuitka --standalone --recurse-all --include-package=OpenGL --windows-icon-from-ico="starshot.ico" --windows-disable-console starshot.py
